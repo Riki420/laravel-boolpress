ea751dda19a125d4b6e357ac073648790d2d5b9c
Riki420 <girottiriccardo1998@gmail.com>
2021-10-19 16:46:05 +0200

    added button for edit and destroy

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,11 +5,14 @@
 <div class="container">
     <table class="table">
         <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Post Title</th>
-            <th scope="col">Created At</th>
-          </tr>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Post Title</th>
+                <th scope="col">Created At</th>
+                <th class="d-flex justify-content-end">
+                    <a href="{{ route('admin.posts.create') }}" class="btn btn-success">Add New Post</a>
+                </th>
+            </tr>
         </thead>
         <tbody>
           @forelse($posts as $post)
@@ -18,7 +21,9 @@
                 <td>{{$post->title}}</td>
                 <td>{{$post->created_at}}</td>
                 <td class="d-flex justify-content-end">
-                  <a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">Visualizza</a>
+                  <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary">View Post </a>
+                  <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-warning ml-2">Edit Post </a>
+                  <a href="{{ route('admin.posts.destroy', $post->id) }}" class="btn btn-danger ml-2">Delete Post </a>
 
                 </td>
               </tr>

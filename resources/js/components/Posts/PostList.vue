3765eb57cf93ea6ede1aff3ccd35cac7eb36c210
Riki420 <girottiriccardo1998@gmail.com>
2021-10-22 12:35:29 +0200

    @click currentPage +1, currentPage - 1

--- resources/js/components/Posts/PostList.vue
+++ resources/js/components/Posts/PostList.vue
@@ -2,14 +2,22 @@
     <section id="postList" class="mt-4">
         <h2>Post</h2>
         <PostCard v-for="post in posts" :key="post.id" :post="post" />
-        <nav aria-label="Page navigation example" class="mt-4">
+        <nav
+            aria-label="Page navigation example"
+            class="mt-4 d-flex justify-content-end"
+        >
             <ul class="pagination">
-                <li class="page-item" v-if="pagination.current_page > 1">
+                <li
+                    class="page-item"
+                    v-if="pagination.currentPage > 1"
+                    @click="getPosts(pagination.currentPage - 1)"
+                >
                     <a class="page-link" href="#">Previous</a>
                 </li>
                 <li
                     class="page-item"
-                    v-if="pagination.current_page === pagination.last_page"
+                    v-if="pagination.lastPage > pagination.currentPage"
+                    @click="getPosts(pagination.currentPage + 1)"
                 >
                     <a class="page-link" href="#">Next</a>
                 </li>
@@ -33,9 +41,9 @@ export default {
     },
     methods: {
         //getPost
-        getPosts() {
+        getPosts(page) {
             axios
-                .get("http://localhost:8000/api/posts")
+                .get(`http://localhost:8000/api/posts?page=${page}`)
                 .then(res => {
                     //recupero le proprietà data [Destructuring]
                     const { data, current_page, last_page } = res.data;
@@ -56,4 +64,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.page-item {
+    cursor: pointer;
+    .page-link {
+        color: #000;
+    }
+}
+</style>
